5f2c3630afe4efe99999b858bb47541d8b70d8ca
GiaSen <giammasena@gmail.com>
2019-12-25 17:26:49 +0100

    kunlun2: Import Lenovo changes
     Source https://lenovomobilesupport.lenovo.com/nz/en/products/phones/k-series/k10-note/downloads/ds541914

--- drivers/soc/qcom/icnss.c
+++ drivers/soc/qcom/icnss.c
@@ -2315,7 +2315,6 @@ static int icnss_call_driver_probe(struct icnss_priv *priv)
 
 	icnss_hw_power_on(priv);
 
-	icnss_block_shutdown(true);
 	while (probe_cnt < ICNSS_MAX_PROBE_CNT) {
 		ret = priv->ops->probe(&priv->pdev->dev);
 		probe_cnt++;
@@ -2325,11 +2324,9 @@ static int icnss_call_driver_probe(struct icnss_priv *priv)
 	if (ret < 0) {
 		icnss_pr_err("Driver probe failed: %d, state: 0x%lx, probe_cnt: %d\n",
 			     ret, priv->state, probe_cnt);
-		icnss_block_shutdown(false);
 		goto out;
 	}
 
-	icnss_block_shutdown(false);
 	set_bit(ICNSS_DRIVER_PROBED, &priv->state);
 
 	return 0;
@@ -2580,7 +2577,6 @@ static int icnss_driver_event_register_driver(void *data)
 	if (ret)
 		goto out;
 
-	icnss_block_shutdown(true);
 	while (probe_cnt < ICNSS_MAX_PROBE_CNT) {
 		ret = penv->ops->probe(&penv->pdev->dev);
 		probe_cnt++;
@@ -2590,11 +2586,9 @@ static int icnss_driver_event_register_driver(void *data)
 	if (ret) {
 		icnss_pr_err("Driver probe failed: %d, state: 0x%lx, probe_cnt: %d\n",
 			     ret, penv->state, probe_cnt);
-		icnss_block_shutdown(false);
 		goto power_off;
 	}
 
-	icnss_block_shutdown(false);
 	set_bit(ICNSS_DRIVER_PROBED, &penv->state);
 
 	return 0;
@@ -4337,9 +4331,6 @@ static int icnss_stats_show_state(struct seq_file *s, struct icnss_priv *priv)
 		case ICNSS_DRIVER_UNLOADING:
 			seq_puts(s, "DRIVER UNLOADING");
 			continue;
-		case ICNSS_MODE_ON:
-			seq_puts(s, "MODE ON DONE");
-			continue;
 		case ICNSS_BLOCK_SHUTDOWN:
 			seq_puts(s, "BLOCK SHUTDOWN");
 			continue;
@@ -4349,6 +4340,8 @@ static int icnss_stats_show_state(struct seq_file *s, struct icnss_priv *priv)
 		case ICNSS_MODEM_CRASHED:
 			seq_puts(s, "MODEM CRASHED");
 			continue;
+		case ICNSS_MODE_ON:
+			seq_puts(s, "MODE ON DONE");
 		}
 
 		seq_printf(s, "UNKNOWN-%d", i);
@@ -5020,8 +5013,6 @@ static int icnss_probe(struct platform_device *pdev)
 
 	penv = priv;
 
-	init_completion(&priv->unblock_shutdown);
-
 	icnss_pr_info("Platform driver probed successfully\n");
 
 	return 0;
@@ -5043,8 +5034,6 @@ static int icnss_remove(struct platform_device *pdev)
 	device_init_wakeup(&penv->pdev->dev, false);
 
 	icnss_debugfs_destroy(penv);
-
-	complete_all(&penv->unblock_shutdown);
 
 	icnss_modem_ssr_unregister_notifier(penv);
 
